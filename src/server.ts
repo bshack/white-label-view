3ac49c7e78201d895c3b64c1fb63ffaef9880032
William Shackelford <bshackelford@gmail.com>
2026-09-14 15:34:27 -0400

    Align server View typed model event contract

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import type {JSXMarkup} from './jsx-runtime.js';
 /** Data source consumed by the server rendering lifecycle. */
 interface ViewModel {
     get?: () => unknown;
-    on?: (event: string, callback: () => void) => unknown;
-    removeListener?: (event: string, callback: () => void) => unknown;
+    on?: (event: 'change', callback: () => void) => unknown;
+    removeListener?: (event: 'change', callback: () => void) => unknown;
 }
 
 /** Server settings mirror the portable subset of browser View settings. */
