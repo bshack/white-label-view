6838abda2ccce12b720474e1bb32688630ab7376
William Shackelford <bshackelford@gmail.com>
2026-09-15 17:42:26 -0400

    Test EventTarget-only model binding

--- test/eventtarget-model-binding.test.js
+++ test/eventtarget-model-binding.test.js
@@ -14,12 +14,10 @@ function eventTargetModel(value) {
     const model = new EventTarget();
     model.value = value;
     model.get = () => ({value: model.value});
-    model.on = () => {throw new Error('legacy on() must not be used when EventTarget is available');};
-    model.removeListener = () => {throw new Error('legacy removeListener() must not be used when EventTarget is available');};
     return model;
 }
 
-test('browser View prefers EventTarget model binding and releases it on destroy', t => {
+test('browser View binds EventTarget models and releases them on destroy', t => {
     browserDom(t);
     const View = require('../dist');
     const parentElement = document.querySelector('main');
@@ -37,7 +35,7 @@ test('browser View prefers EventTarget model binding and releases it on destroy'
     assert.equal(parentElement.childNodes.length, 0);
 });
 
-test('server View prefers EventTarget model binding and releases it on destroy', () => {
+test('server View binds EventTarget models and releases them on destroy', () => {
     const View = require('../dist/server');
     const model = eventTargetModel('first');
     const view = new View({model, template: data => `<main>${data.value}</main>`}).initialize();
