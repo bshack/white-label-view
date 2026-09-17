0267ef798be47ce6ba23f0ee9f00c3a527fb3020
William Shackelford <bshackelford@gmail.com>
2026-09-16 23:46:49 -0400

    Test tagged HTML package export

--- test/package-exports.test.js
+++ test/package-exports.test.js
@@ -6,12 +6,11 @@ const assert = require('node:assert/strict');
 test('public package entrypoints resolve from built output', () => {
     const View = require('white-label-view');
     const ServerView = require('white-label-view/server');
-    const jsxRuntime = require('white-label-view/jsx-runtime');
-    const jsxDevRuntime = require('white-label-view/jsx-dev-runtime');
+    const htmlRuntime = require('white-label-view/html');
 
     assert.equal(typeof View, 'function');
     assert.equal(typeof ServerView, 'function');
-    assert.equal(typeof jsxRuntime.jsx, 'function');
-    assert.equal(typeof jsxRuntime.jsxs, 'function');
-    assert.equal(typeof jsxDevRuntime.jsxDEV, 'function');
+    assert.equal(typeof htmlRuntime.html, 'function');
+    assert.equal(typeof htmlRuntime.attributes, 'function');
+    assert.equal(typeof htmlRuntime.unsafeHTML, 'function');
 });
